4b9ef295201fc16034688e41959915ec65f9b99b
dirocCen <15521284374@163.com>
2023-08-21 16:37:30 +0200

    update inertia tensor

--- src/pulgon_tools_wip/detect_point_group.py
+++ src/pulgon_tools_wip/detect_point_group.py
@@ -3,12 +3,48 @@ from pdb import set_trace
 
 import numpy as np
 import pretty_errors
+from ase.data import atomic_masses
 from ase.io import read
 from pymatgen.core import Molecule
 from pymatgen.core.operations import SymmOp
 from pymatgen.symmetry.analyzer import PointGroupAnalyzer
 
-from pulgon_tools_wip.generate_structures import sigmaV
+
+def _inertia_tensor(self):
+    weights = np.array([site.species.weight for site in self.centered_mol])
+    Ixx = np.sum(
+        weights * np.sum(self.centered_mol.cart_coords[:, 1:] ** 2, axis=1)
+    )
+    Iyy = np.sum(
+        weights * np.sum(self.centered_mol.cart_coords[:, [0, 2]] ** 2, axis=1)
+    )
+    Izz = np.sum(
+        weights * np.sum(self.centered_mol.cart_coords[:, :2] ** 2, axis=1)
+    )
+    Ixy = -1 * np.sum(
+        weights
+        * self.centered_mol.cart_coords[:, 0]
+        * self.centered_mol.cart_coords[:, 1]
+    )
+    Ixz = -1 * np.sum(
+        weights
+        * self.centered_mol.cart_coords[:, 0]
+        * self.centered_mol.cart_coords[:, 2]
+    )
+    Iyz = -1 * np.sum(
+        weights
+        * self.centered_mol.cart_coords[:, 1]
+        * self.centered_mol.cart_coords[:, 2]
+    )
+    inertia_tensor = np.array(
+        [[Ixx, Ixy, Izz], [Ixy, Iyy, Iyz], [Ixz, Iyz, Izz]]
+    )
+
+    total_inertia = np.sum(
+        weights * np.sum(self.centered_mol.cart_coords**2, axis=1)
+    )
+    inertia_tensor = inertia_tensor / total_inertia
+    return inertia_tensor
 
 
 class LineGroupAnalyzer(PointGroupAnalyzer):
@@ -25,22 +61,9 @@ class LineGroupAnalyzer(PointGroupAnalyzer):
             self.sch_symbol = "Cs"
 
     def _analyze(self):
-        inertia_tensor = np.zeros((3, 3))
-        total_inertia = 0
-        for site in self.centered_mol:
-            c = site.coords
-            wt = site.species.weight
-            for i in range(3):
-                inertia_tensor[i, i] += wt * (
-                    c[(i + 1) % 3] ** 2 + c[(i + 2) % 3] ** 2
-                )
-            for i, j in [(0, 1), (1, 2), (0, 2)]:
-                inertia_tensor[i, j] += -wt * c[i] * c[j]
-                inertia_tensor[j, i] += -wt * c[j] * c[i]
-            total_inertia += wt * np.dot(c, c)
-        inertia_tensor /= total_inertia
-        eigvals, eigvecs = np.linalg.eig(inertia_tensor)
-        self.principal_axes = eigvecs.T
+        inertia_tensor = _inertia_tensor(self)
+        _, eigvecs = np.linalg.eigh(inertia_tensor)
+        self.principal_axes = eigvecs.T  # only be used in _proc_no_rot_sym
 
         self.rot_sym = []
         self.symmops = [SymmOp(np.eye(4))]
