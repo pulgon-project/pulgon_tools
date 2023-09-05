09d5df20d09489c90fdfa735a8e843d1f29451df
dirocCen <15521284374@163.com>
2023-09-05 09:09:57 +0200

    update

--- test/test_dection_func.py
+++ test/test_dection_func.py
@@ -16,7 +16,7 @@ MODULE_DIR = Path(__file__).absolute().parent
 
 class TestCyclicGroupAnalyzer:
     def test_find_axis_center_of_nanotube(self):
-        st_name = os.path.join(MODULE_DIR, "data", "12-12-AM.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "12-12-AM")
         st = read_vasp(st_name)
         res = CyclicGroupAnalyzer(st)
         n_st = res._find_axis_center_of_nanotube(st)
@@ -24,7 +24,7 @@ class TestCyclicGroupAnalyzer:
         assert (average_coord - [0, 0]).sum() < 0.001
 
     def test_generate_monomer(self):
-        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM")
         st = read_vasp(st_name)
         cy = CyclicGroupAnalyzer(st)
         monomers, translations = cy._potential_translation()
@@ -32,7 +32,7 @@ class TestCyclicGroupAnalyzer:
         assert translations[0] == 0.5
 
     def test_rotation(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st1.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st1")
         st = read_vasp(st_name)
         cy = CyclicGroupAnalyzer(st)
         monomers, translations = cy._potential_translation()
@@ -43,7 +43,7 @@ class TestCyclicGroupAnalyzer:
         assert Q == 12
 
     def test_rotational_tolerance(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st1.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st1")
         st = read_vasp(st_name)
         cy1 = CyclicGroupAnalyzer(st, symprec=1e-4)
         cy2 = CyclicGroupAnalyzer(st, symprec=1e-5)
@@ -59,7 +59,7 @@ class TestCyclicGroupAnalyzer:
         assert idx2 == False and Q2 == 1
 
     def test_mirror(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st7.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st7")
         st = read_vasp(st_name)
         cy = CyclicGroupAnalyzer(st)
         monomers, translations = cy._potential_translation()
@@ -69,7 +69,7 @@ class TestCyclicGroupAnalyzer:
         assert idx == True
 
     def test_mirror_tolerance(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st7.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st7")
         st = read_vasp(st_name)
 
         cy1 = CyclicGroupAnalyzer(st, symprec=1e-15)
@@ -86,21 +86,21 @@ class TestCyclicGroupAnalyzer:
         assert idx2 == False
 
     def test_the_whole_function_st1(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st1.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st1")
         st = read_vasp(st_name)
         cyclic = CyclicGroupAnalyzer(st)
         cy, mon = cyclic.get_cyclic_group()
         assert cy[0] == "T12(1.5)" and str(mon[0].symbols) == "C4"
 
     def test_the_whole_function_st2(self):
-        st_name = os.path.join(MODULE_DIR, "data", "st7.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "st7")
         st = read_vasp(st_name)
         cyclic = CyclicGroupAnalyzer(st)
         cy, mon = cyclic.get_cyclic_group()
         assert cy[0] == "T'(1.5)" and str(mon[0].symbols) == "C6"
 
     def test_the_whole_function_st3(self):
-        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM")
         st = read_vasp(st_name)
         cyclic = CyclicGroupAnalyzer(st)
         cy, mon = cyclic.get_cyclic_group()
@@ -133,7 +133,7 @@ class TestAxialPointGroupAnalyzer:
         assert str(pg) == "D6"
 
     def test_axial_pg_st4(self):
-        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "9-9-AM")
         st = read_vasp(st_name)
         mol = Molecule(species=st.numbers, coords=st.positions)
         obj = LineGroupAnalyzer(mol)
@@ -141,7 +141,7 @@ class TestAxialPointGroupAnalyzer:
         assert str(pg) == "S2"
 
     def test_axial_pg_st6(self):
-        st_name = os.path.join(MODULE_DIR, "data", "C4h.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "C4h")
         st = read_vasp(st_name)
         mol = Molecule(species=st.numbers, coords=st.positions)
         obj = LineGroupAnalyzer(mol)
@@ -149,7 +149,7 @@ class TestAxialPointGroupAnalyzer:
         assert str(pg) == "C4h"
 
     def test_axial_pg_st7(self):
-        st_name = os.path.join(MODULE_DIR, "data", "C4v.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "C4v")
         st = read_vasp(st_name)
         mol = Molecule(species=st.numbers, coords=st.positions)
         obj = LineGroupAnalyzer(mol)
@@ -157,7 +157,7 @@ class TestAxialPointGroupAnalyzer:
         assert str(pg) == "C4v"
 
     def test_axial_pg_st8(self):
-        st_name = os.path.join(MODULE_DIR, "data", "C4.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "C4")
         st = read_vasp(st_name)
         mol = Molecule(species=st.numbers, coords=st.positions)
         obj = LineGroupAnalyzer(mol)
@@ -165,7 +165,7 @@ class TestAxialPointGroupAnalyzer:
         assert str(pg) == "C4"
 
     def test_axial_pg_st9(self):
-        st_name = os.path.join(MODULE_DIR, "data", "non-sym.vasp")
+        st_name = os.path.join(MODULE_DIR, "data", "non-sym")
         st = read_vasp(st_name)
         mol = Molecule(species=st.numbers, coords=st.positions)
         obj = LineGroupAnalyzer(mol)
