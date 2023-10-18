2aab10a90a2a0e3fa8e7c548a240a5266d8e40c9
dirocCen <15521284374@163.com>
2023-10-18 18:39:24 +0200

    update symcell

--- src/pulgon_tools_wip/test.py
+++ src/pulgon_tools_wip/test.py
@@ -1,17 +1,3 @@
-# Copyright 2023 The PULGON Project Developers
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# permissions and limitations under the License.
-
 import argparse
 import json
 import pickle
@@ -25,15 +11,20 @@ from ase.io.vasp import read_vasp, write_vasp
 from pulgon_tools_wip.detect_generalized_translational_group import (
     CyclicGroupAnalyzer,
 )
+from pulgon_tools_wip.detect_point_group import LineGroupAnalyzer
 
 
 def get_symcell(monomer):
-
-    set_trace()
+    apg = LineGroupAnalyzer(monomer)
+    equ = list(apg.get_equivalent_atoms()["eq_sets"])
+    # sym = apg.get_symmetry_operations()
+    # write_vasp("monomer.vasp", monomer)
+    # write_vasp("symcell.vasp", monomer[equ])
+    return monomer[equ]
 
 
 def main():
-    poscar = read_vasp("12-12-AM.vasp")
+    poscar = read_vasp("POSCAR")
     cyclic = CyclicGroupAnalyzer(poscar, corner=True, symprec=0.01)
     cy, mon = cyclic.get_cyclic_group()
 
