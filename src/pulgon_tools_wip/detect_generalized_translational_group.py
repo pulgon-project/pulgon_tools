33e4a1bbafdbb86d1a6945e5d995b2bbbbebbfd2
dirocCen <15521284374@163.com>
2023-10-20 14:36:20 +0200

    update typo

--- src/pulgon_tools_wip/detect_generalized_translational_group.py
+++ src/pulgon_tools_wip/detect_generalized_translational_group.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http:#www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -271,8 +271,8 @@ class CyclicGroupAnalyzer:
                 itp1 = itp1 and np.array(itp3).all()
                 itp2 = itp2 and np.array(itp4).all()
 
-                if not (itp1 or itp2):
-                    break
+            if not (itp1 or itp2):
+                break
 
             if itp1 or itp2:
                 Q = int(360 / test_ind)
--- src/pulgon_tools_wip/utils.py
+++ src/pulgon_tools_wip/utils.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http:#www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -104,6 +104,6 @@ def get_symcell(monomer: Atoms) -> Atoms:
 
     """
     apg = LineGroupAnalyzer(monomer)
-    equ = list(apg.get_equivalent_atoms()["eq_sets"])
+    equ = list(apg.get_equivalent_atoms()["eq_sets"].keys())
     # sym = apg.get_symmetry_operations()
     return monomer[equ]
